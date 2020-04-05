b8069214c1d7a139e6f805e6e81d1410745ba1a9
NNemanjaMM <nemanja.miladinovic@live.com>
2020-04-05 22:38:16 +0200

    members: filter function added for most of the fields, filter input validation added

--- elibrary/members/routes.py
+++ elibrary/members/routes.py
@@ -9,6 +9,7 @@ from elibrary.members.forms import (MemberCreateForm, MemberUpdateForm,
     UserExtensionForm, FilterForm, ShortFilterForm)
 from elibrary.utils.numeric_defines import MEMBERSHIP_EXTENSION_DAYS, PAGINATION
 from elibrary.main.forms import AcceptForm, RejectForm
+from elibrary.utils.custom_validations import (string_cust, length_cust_max, FieldValidator)
 
 members = Blueprint('members', __name__)
 
@@ -96,27 +97,159 @@ def members_extend(member_id):
 
 @members.route("/members")
 @login_required
-def memberss():
+def memberss(filtering = False, searching = False):
     page = request.args.get('page', 1, type=int)
     sort_criteria = request.args.get('sort_by', 'first_name', type=str)
     sort_direction = request.args.get('direction', 'up', type=str)
     args_sort = {'sort_by': sort_criteria, 'direction': sort_direction}
 
+    s_text = request.args.get('text')
+    f_registration_date_from = request.args.get('registration_date_from')
+    f_registration_date_to = request.args.get('registration_date_to')
+    f_expiration_date_from = request.args.get('expiration_date_from')
+    f_expiration_date_to = request.args.get('expiration_date_to')
+    f_birth_date_from = request.args.get('birth_date_from')
+    f_birth_date_to = request.args.get('birth_date_to')
+    f_books_rented_from = request.args.get('books_rented_from')
+    f_books_rented_to = request.args.get('books_rented_to')
+    f_first_name = request.args.get('first_name')
+    f_last_name = request.args.get('last_name')
+    f_has_rented_books = request.args.get('has_rented_books')
+    f_has_expired = request.args.get('has_expired')
+    if filtering:
+        s_text = None
+    elif searching:
+        f_registration_date_from = None
+        f_registration_date_to = None
+        f_expiration_date_from = None
+        f_expiration_date_to = None
+        f_birth_date_from = None
+        f_birth_date_to = None
+        f_books_rented_from = None
+        f_books_rented_to = None
+        f_first_name = None
+        f_last_name = None
+        f_has_rented_books = None
+        f_has_expired = None
+
+    filter_has_errors = False
     args_filter = {}
     form = FilterForm()
     form2 = ShortFilterForm()
     my_query = db.session.query(Member)
 
-    tekst = request.args.get('name')
-    if not (tekst == None or tekst == ""):
-        form2.name.data = tekst
-        my_query = my_query.filter(or_(Member.first_name.like('%' + tekst + '%'), Member.last_name.like('%' + tekst + '%')))
-        args_filter['name'] = tekst
+    if not (s_text == None or s_text == ""):
+        form2.text.data = s_text
+        if FieldValidator.validate_field(form2, form2.text, [string_cust]):
+            my_query = my_query.filter(or_(Member.first_name.like('%' + s_text + '%'), Member.last_name.like('%' + s_text + '%')))
+            args_filter['name'] = s_text
+    else:
+        my_query, args_filter, filter_has_errors = process_related_date_filters(my_query,
+            args_filter, filter_has_errors, form.registration_date_from,
+            form.registration_date_to, f_registration_date_from, f_registration_date_to,
+            'registration_date_from', 'registration_date_to', 'date_registered', True)
 
+        my_query, args_filter, filter_has_errors = process_related_date_filters(my_query,
+            args_filter, filter_has_errors, form.expiration_date_from,
+            form.expiration_date_to, f_expiration_date_from, f_expiration_date_to,
+            'expiration_date_from', 'expiration_date_to', 'date_expiration', False)
+
+        my_query, args_filter, filter_has_errors = process_related_date_filters(my_query,
+            args_filter, filter_has_errors, form.birth_date_from,
+            form.birth_date_to, f_birth_date_from, f_birth_date_to,
+            'birth_date_from', 'birth_date_to', 'birth_date', True)
+
+        my_query, args_filter, filter_has_errors = process_related_number_filters(my_query,
+            args_filter, filter_has_errors, form.books_rented_from,
+            form.books_rented_to, f_books_rented_from, f_books_rented_to,
+            'books_rented_from', 'books_rented_to', 'total_books_rented')
+
+        if not (f_first_name == None or f_first_name == ""):
+            form.first_name.data = f_first_name
+            if FieldValidator.validate_field(form, form.first_name, [string_cust, length_cust_max]):
+                my_query = my_query.filter(Member.first_name.like('%' + f_first_name + '%'))
+                args_filter['first_name'] = f_first_name
+            else:
+                filter_has_errors = True
+
+        if not (f_last_name == None or f_last_name == ""):
+            form.last_name.data = f_last_name
+            if FieldValidator.validate_field(form, form.last_name, [string_cust, length_cust_max]):
+                my_query = my_query.filter(Member.last_name.like('%' + f_last_name + '%'))
+                args_filter['last_name'] = f_last_name
+            else:
+                filter_has_errors = True
+
+
+    if filter_has_errors:
+        flash(_l('There are filter values with errors')+'. '+_l('However, valid filter values are applied')+'.', 'warning')
     if sort_direction == 'up':
         list = my_query.order_by(sort_criteria).paginate(page=page, per_page=PAGINATION)
     else:
         list = my_query.order_by(desc(sort_criteria)).paginate(page=page, per_page=PAGINATION)
-
     args_filter_and_sort = {**args_filter, **args_sort}
     return render_template('members.html', form=form, form2=form2, members_list=list, extra_filter_args=args_filter, extra_sort_and_filter_args=args_filter_and_sort)
+
+@members.route("/membersr")
+@login_required
+def membersr():
+    return memberss(False, True)
+
+@members.route("/membersf")
+@login_required
+def membersf():
+    return memberss(True, False)
+
+def process_related_date_filters(my_query, args_filter, filter_has_errors, from_field, to_field, f_from, f_to, field_from_name, field_to_name, db_column_name, validate_future_date):
+    from_value = None
+    if not (f_from == None or f_from == ""):
+        from_field.data = f_from
+        from_value = FieldValidator.convert_and_validate_date(from_field, validate_future_date)
+        if not from_value == None:
+            my_query = my_query.filter(getattr(Member, db_column_name) >= from_value.strftime('%Y-%m-%d'))
+            args_filter[field_from_name] = f_from
+        else:
+            filter_has_errors = True
+    if not (f_to == None or f_to == ""):
+        to_field.data = f_to
+        to_value = FieldValidator.convert_and_validate_date(to_field, validate_future_date)
+        if not to_value == None:
+            if not from_value == None:
+                if FieldValidator.validate_date_order(from_value, to_value, to_field):
+                    my_query = my_query.filter(getattr(Member, db_column_name) <= to_value.strftime('%Y-%m-%d'))
+                    args_filter[field_to_name] = f_to
+                else:
+                    filter_has_errors = True
+            else:
+                my_query = my_query.filter(getattr(Member, db_column_name) <= to_value.strftime('%Y-%m-%d'))
+                args_filter[field_to_name] = f_to
+        else:
+            filter_has_errors = True
+    return my_query, args_filter, filter_has_errors
+
+def process_related_number_filters(my_query, args_filter, filter_has_errors, from_field, to_field, f_from, f_to, field_from_name, field_to_name, db_column_name):
+    from_value = None
+    if not (f_from == None or f_from == ""):
+        from_field.data = f_from
+        from_value = FieldValidator.convert_and_validate_number(from_field)
+        if not from_value == None:
+            my_query = my_query.filter(getattr(Member, db_column_name) >= from_value)
+            args_filter[field_from_name] = f_from
+        else:
+            filter_has_errors = True
+    if not (f_to == None or f_to == ""):
+        to_field.data = f_to
+        to_value = FieldValidator.convert_and_validate_number(to_field)
+        if not to_value == None:
+            if not from_value == None:
+                if FieldValidator.validate_number_order(from_value, to_value, to_field):
+                    my_query = my_query.filter(getattr(Member, db_column_name) <= to_value)
+                    args_filter[field_to_name] = f_to
+                else:
+                    filter_has_errors = True
+            else:
+                my_query = my_query.filter(getattr(Member, db_column_name) <= to_value)
+                args_filter[field_to_name] = f_to
+        else:
+            filter_has_errors = True
+    return my_query, args_filter, filter_has_errors
--- elibrary/utils/custom_validations.py
+++ elibrary/utils/custom_validations.py
@@ -1,5 +1,7 @@
 from wtforms.validators import DataRequired, Email, Length, EqualTo, Regexp, optional
 from flask_babel import lazy_gettext as _l
+from datetime import datetime, date
+from elibrary.utils.numeric_defines import MINIMUM_DATE, MAXIMUM_NUMBER
 
 class required_cust(DataRequired):
     def __init__(self, text = _l('The field must be filled')+'.'):
@@ -43,3 +45,68 @@ class length_cust(Length):
         else:
             text = _l('Minimum input length must be') + ' ' + str(min) + ' ' + _l('characters') +'.'
         Length.__init__(self, min, max, message = text)
+
+class length_cust_max(Length):
+    def __init__(self):
+        text = _l('Input length can not be greather than') + ' 40 ' + _l('characters') + '.'
+        Length.__init__(self, -1, 40, message = text)
+
+class FieldValidator():
+    @staticmethod
+    def validate_field(form, field, validator_classes):
+        field.errors = list()
+        for validator_class in validator_classes:
+            validator = validator_class()
+            try:
+                validator(form, field)
+            except ValueError as e:
+                field.errors.append(e.args[0])
+        return len(field.errors) == 0
+
+    @staticmethod
+    def convert_and_validate_date(field, allow_future):
+        value = None
+        field.errors = list()
+        try:
+            value = datetime.strptime(field.data, "%d.%m.%Y.").date()
+        except ValueError:
+            field.errors.append(_l('Date value is not valid') + '. ' + _l('Make sute if matches the following format') + ' "dd.mm.yyyy.".')
+        if not value == None:
+            if value < datetime.strptime(MINIMUM_DATE, "%d.%m.%Y.").date():
+                field.errors.append(_l('Date can not be set before') + ' "' + str(MINIMUM_DATE) + '".')
+                value = None
+            elif allow_future and value > date.today():
+                field.errors.append(_l('Date can not be set in future') + '.')
+                value = None
+        return value
+
+    @staticmethod
+    def validate_date_order(first_date_value, second_date_value, second_date_field):
+        second_date_field.errors = list()
+        if not first_date_value < second_date_value:
+            second_date_field.errors.append(_l('Before date value can not be set after the after date value') + '.')
+        return len(second_date_field.errors) == 0
+
+    @staticmethod
+    def convert_and_validate_number(field):
+        value = None
+        field.errors = list()
+        try:
+            value = int(field.data)
+        except ValueError:
+            field.errors.append(_l('Value is not a valid whole number') + '.')
+        if not value == None:
+            if value < 0:
+                field.errors.append(_l('Number can not be lower than zero') + '.')
+                value = None
+            elif value > MAXIMUM_NUMBER:
+                field.errors.append(_l('Number can not be greater than') + ' ' + str(MAXIMUM_NUMBER) + '.')
+                value = None
+        return value
+
+    @staticmethod
+    def validate_number_order(first_number_value, second_number_value, second_number_field):
+        second_number_field.errors = list()
+        if not first_number_value < second_number_value:
+            second_number_field.errors.append(_l('Greater than value can not be higher than lower than value') + '.')
+        return len(second_number_field.errors) == 0
